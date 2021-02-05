06b758ce1d3f445e7749810c3ded64b929bb2006
rbardinet <rbardine@enssat.fr>
2021-02-05 11:49:21 +0100

    Initial commit

---
+++ src/main/java/org/tutorial/HelloWorldServlet.java
@@ -0,0 +1,27 @@
+package org.tutorial;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends javax.servlet.http.HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+
+        out.println("<head><title>Hello World</title></head>");
+
+        out.println("<body>");
+
+        out.println("<h1>Hello World !</h1>");
+
+        out.println("</body></html>");
+
+    }
+}
---
+++ target/Enssat-1.0-SNAPSHOT/WEB-INF/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rkbcht
+  Date: 2/5/2021
+  Time: 11:42 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>BONJOUR LA BRETAGNE</title>
+</head>
+<body>
+    <h1>Bonjour la Bretagne</h1>
+</body>
+</html>
