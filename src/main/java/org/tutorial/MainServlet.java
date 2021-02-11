c53affa2ce151edcbfb4fd6463935271d96a44f3
RB <rbardine@enssat.fr>
2021-02-12 00:19:09 +0100

    u

--- src/main/java/org/tutorial/MainServlet.java
+++ src/main/java/org/tutorial/MainServlet.java
@@ -1,4 +1,27 @@
 package org.tutorial;
 
-public class MainServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainServlet extends javax.servlet.http.HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+
+        out.println("<head><title>Hello World</title></head>");
+
+        out.println("<body>");
+
+        out.println("<h1>Hello World !</h1>");
+
+        out.println("</body></html>");
+
+    }
 }
