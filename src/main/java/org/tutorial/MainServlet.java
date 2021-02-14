8276cb18221f759330208fcfdb3f690bb839d414
RB <rbardine@enssat.fr>
2021-02-14 23:33:09 +0100

    added branch
    added branch biblio and started biblio

--- src/main/java/org/tutorial/MainServlet.java
+++ src/main/java/org/tutorial/MainServlet.java
@@ -1,5 +1,6 @@
 package org.tutorial;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,21 +8,41 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MainServlet extends javax.servlet.http.HttpServlet{
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.setContentType("text/html");
-
-        PrintWriter out = response.getWriter();
-
-        out.println("<html>");
-
-        out.println("<head><title>Hello World</title></head>");
-
-        out.println("<body>");
-
-        out.println("<h1>Hello World !</h1>");
-
-        out.println("</body></html>");
+        doProcess(request,response);
 
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        doProcess(request,response);
+
+    }
+
+
+    private void doProcess(HttpServletRequest request, HttpServletResponse response) {
+
+        String pageName="/accueil.jsp";
+
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
+
+        try {
+
+            rd.forward(request, response);
+
+        } catch (ServletException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+
+
 }
