354e85145bf45a634555b1f5f6f3525b12d52004
rbardinet <rbardine@enssat.fr>
2021-02-20 16:39:01 +0100

    ca marche?

--- src/main/java/org/tutorial/MainServlet.java
+++ src/main/java/org/tutorial/MainServlet.java
@@ -1,5 +1,6 @@
 package org.tutorial;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,21 +8,38 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MainServlet extends javax.servlet.http.HttpServlet{
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.setContentType("text/html");
+        doProcess(request, response);
 
-        PrintWriter out = response.getWriter();
+    }
 
-        out.println("<html>");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        out.println("<head><title>Hello World</title></head>");
+        doProcess(request, response);
 
-        out.println("<body>");
+    }
 
-        out.println("<h1>Hello World !</h1>");
+    private void doProcess(HttpServletRequest request, HttpServletResponse response) {
 
-        out.println("</body></html>");
+        String pageName="/accueil.jsp";
+
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
+
+        try {
+
+            rd.forward(request, response);
+
+        } catch (ServletException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
 
     }
 }
---
+++ src/main/webapp/accueil.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rkbcht
+  Date: 2/20/2021
+  Time: 4:34 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>LeTitre</title>
+</head>
+<body>
+    <h1>accueil</h1>
+</body>
+</html>
